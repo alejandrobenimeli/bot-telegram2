6d5968cb27e05888c13acea0aa00836671de22ec
alejandrobenimeli <alejandro.nospam@gmail.com>
2023-03-09 23:33:48 +0100

    aa

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ bot.start((ctx) => {
   // Mostrar el menú fijo
   console.log('el contexto es: '+ctx.message.text);
   console.log('el id de usuario de telegram es: '+ctx.from.id);
+  bot.telegram.sendMessage(5997313040, 'tu madre es calva y lleva perilla');
   ctx.reply('¡Bienvenido! ¿Qué acción quieres realizar?', {
     reply_markup: {
       keyboard: staticMenu,
